b082e69ad8a72030e9ff08c8e2df9c59566575ab
vaibhavk98 <kulkarniva98@gmail.com>
2020-11-13 11:42:38 +0530

    Third commit (added deployment.yml)

--- models/example/employee_model.sql
+++ models/example/employee_model.sql
@@ -2,10 +2,10 @@
     config(materialized="view") }}
 
 create table demofromdbt(id int,employee_name varchar,department varchar,age int);
-insert into demofromdbt values(10,"Alex","IT",26);
-insert into demofromdbt values(11,"Casey","Sales",23);
-insert into demofromdbt values(12,"Robert","CRM",32);
-insert into demofromdbt values(13,"Ronald","Recruitment",29);
-insert into demofromdbt values(14,"Emma","Production",27);
+insert into demofromdbt values(10,'Alex','IT',26);
+insert into demofromdbt values(11,'Casey','Sales',23);
+insert into demofromdbt values(12,'Robert','CRM',32);
+insert into demofromdbt values(13,'Ronald','Recruitment',29);
+insert into demofromdbt values(14,'Emma','Production',27);
 
 select * from demofromdbt where id !=13;
